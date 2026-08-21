33295fefcf91dae6b12d1d9852377adcb6469a0d
felipeauc <felipeau@estudiante.uc.cl>
2026-08-20 21:21:58 -0400

    # Commit 3.1 Creacio nde ltablero y randomizacion de minas

--- BUSCAMINAS --- Juego_3_Alta.py
+++ BUSCAMINAS --- Juego_3_Alta.py
@@ -1,1 +1,21 @@
 # Commit 0.1 Creacion del juego 3
+
+# Commit 3.1
+
+import random
+
+tablero = []
+
+for i in range(5):
+    fila = ["?", "?", "?", "?", "?"]
+    tablero.append(fila)
+
+minas = []
+
+while len(minas) < 5:
+    fila = random.randint(0, 4)
+    columna = random.randint(0, 4)
+    if (fila, columna) not in minas:
+        minas.append((fila, columna))
+        
+print(minas)
